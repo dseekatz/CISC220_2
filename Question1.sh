5646430edf0269c4286930fff8ee35705b290ddc
dseekatz <dseekatz@gmail.com>
2016-10-25 16:34:41 -0400

    Banging my head against the keyboard

--- Question1.sh
+++ Question1.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Get the list of processes and sort them by PPID, then by PID
 oldIFS="$IFS"
 IFS=$'\n'
 processList=($(ps axo comm,pid,ppid,start,user | awk 'NR>1 {print $0}' | sort -n -k 3,3 -k 2,2))
 IFS="$oldIFS"
 
-for (( i=1; i<=${#processList[*]}; i++ )) ; do
+# Get a list of just the process IDs
+for (( i=0; i<=${#processList[*]}; i++ )) ; do
+	processIDs[i]=$(echo "${processList[i]}" | awk '{print $2}')
+done
+
+# Display the tree
+for (( i=0; i<=${#processList[*]}; i++ )) ; do
 	echo "${processList[i]}"
+done
+
+for (( i=0; i<=${#processIDs[*]}; i++ )) ; do
+	echo "${processIDs[i]}"
 done
