b927df50456d524a316400cfa9e6d6d744a5c76c
brookealynn <13brr2@queensu.ca>
2016-10-25 14:35:05 -0500

    New additions

--- Question2.sh
+++ Question2.sh
@@ -3,6 +3,10 @@ Question2
 # This script backs up all the files in the repository, except if the file has previously been backed up, and pushes the backup to the github repository
 # cron job command is used to ensure the script runs everyday at 1:00AM
 
+#Still needed: clarify if userInput or parameters is better to use to get user info.
+#Best way to tell bash not to backup currently backed up files (exclued current date?)
+#Clarify that zip files correct format
+
 #Input path to repository folder, github URL, github username and password
 echo "Please enter the path to the repository folder: "
 read input_variable
@@ -33,4 +37,16 @@ mkdir compressedFiles =/backupfolder
 #help with how this works: tar czvf pathtoCompressedFile /pathToCompress. What about filename?
 tar czvf compressedFiles.tgz fileName /locationOfFile --exclude= <date +%Y-%m-%d-%H) #previously backed up files with current date?
   
+#Commit the new backup file to local git repository
+git commit compressedFiles.tgz
+
+#Push the new backup file to remote git repository
+git push https://github.com/dseekatz/CISC220_2.git --all
+
+#Run the script everyday at 1:00AM and its output redirected to the backupsLog file
+#Create backupLog file
+$ cat > backupLog.sh
+
+#Create run time: daily at 1:00am
+crontab -e 001*** echo >> backupLog.sh
 
