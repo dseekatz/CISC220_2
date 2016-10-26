41c1abddf416186c79d4e8306597336bfd74bb0d
dseekatz <dseekatz@gmail.com>
2016-10-26 09:14:35 -0400

    better formatting for the output

--- Question1.sh
+++ Question1.sh
@@ -3,7 +3,7 @@
 # Get the list of processes and sort them by PPID, then by PID
 oldIFS="$IFS"
 IFS=$'\n'
-processList=($(ps axo comm,pid,ppid,start,user | awk 'NR>1 {print $0}' | sort -n -k 3,3 -k 2,2))
+processList=($(ps axo comm,pid,ppid,start,user | awk 'NR>1 {print $1 " PID:" $2 " PPID:" $3 " Started:" $4 " by:" $5}'))
 IFS="$oldIFS"
 
 # Get a list of just the process IDs
@@ -16,6 +16,6 @@ for (( i=0; i<=${#processList[*]}; i++ )) ; do
 	echo "${processList[i]}"
 done
 
-for (( i=0; i<=${#processIDs[*]}; i++ )) ; do
-	echo "${processIDs[i]}"
-done
+#for (( i=0; i<=${#processIDs[*]}; i++ )) ; do
+#	echo "${processIDs[i]}"
+#done
