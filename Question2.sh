722f657478d23206c36848858181ee9207d676b5
Brookealynn <brookeresendes@outlook.com>
2016-10-31 10:09:30 -0500

    trying to commit

--- Question2.sh
+++ Question2.sh
@@ -1,11 +1,7 @@
-Question2
-
+#Question2
 #!/bin/bash
 # This script backs up all the files in the repository, except if the file has previously been backed up, and pushes the backup to the github repository
-# cron job command is used to ensure the script runs everyday at 1:00AM
-
-#Best way to tell bash not to backup currently backed up files (exclued current date?)
-#Clarify that zip files correct format
+# cron job command is used to ensure the script runs everyday at 1:00am
 
 #Input path to repository folder, github URL, github username and password as parameters
 path=$1
@@ -17,16 +13,13 @@ password=$4
 # tar c:create, z:compress using gzip, v:verbose, f: into file
 
 # Naming backup files where the file name is backup, followed by the current year, month, day and hour
-TIME=$(date + %Y%M%D-%H)
+TIME=$(date + %Y%m%d%-%H)
 
 #The filename will be "backup" and the year/month/day/hour that the file was backed up
 fileName=backup -$TIME.tar.gz
 
-#Destination of backup
-mkdir compressedFiles =/backupfolder 
-
-#help with how this works: tar czvf pathtoCompressedFile /pathToCompress. What about filename?
-tar czvf $fileName / --exclude= backup*.tar.gz #previously backed up files with current date?
+#Compress files
+tar czvf $fileName . --exclude= backup*.tgz #previously backed up files with current dat
 
 #Commit the new backup file to local git repository
 git add compressedFiles.tgz
@@ -38,7 +31,7 @@ git push $1 --all
 #Run the script everyday at 1:00AM and its output redirected to the backupsLog file
 #Create backupLog file
 $ cat > backupLog.sh
-
+echo The file has been backed up to the backupLog
 #Create run time: daily at 1:00am #can just put this in script?
 crontab -e 001*** echo >> backupLog.sh
 
--- Question5.sh
+++ Question5.sh
@@ -1,26 +1,32 @@
-Question5.sh #change to filewallSetup.sh
+#Question5.sh
+#change to filewallSetup.sh
 #!/bin/bash
 #This program adds firewall rules to do various things
 
 #Delete any previous rules in the INPUT, FORWARD and OUTPUT tables
 Sudo iptables -F
 
-#Enable ssh only from computers in the range 130.15.00 to 130.15.255.255
+#Enable ssh only from computers in the range 130.15.00 to 130.15.255.2                              
+sudo iptables -A INPUT -p TCP -sport 22 -m -prange -src-range 130.15.0.0-130.15.255.255 
 
 #Enable ssh from computers in any private network
 sudo iptables -A INPUT -p TCP --sport 22 -m -prange --src-range 192.168.0.0-192.168.255.255 -j ACCEPT
 sudo iptables -A INPUT -p TCP --sport 22 -m iprange --src-range 172.16.0.0-172.31.255.255 -j ACCEPT
 sudo iptables -A INPUT -p TCP --sport 22 -m iprange --src-range 10.0.0.0-10.255.255.255 -j ACCEPT
 
-#Enable http and https from any IP
+#Enable http and https from any IP #should work
+sudo iptables -A INPUT -p TCP -m TCP --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p TCP -m TCP --dport 443 -j ACCEPT
 
-#Drop any other incoming tcp traffic
+#Drop any other incoming tcp traffic #from command sheet
+sudo iptables -A INPUT -j DROP
 
 #Block connecting from your computer to any IP outside the range 130.15.0.0 to 130.15.255.255[netmask/16]
-
+sudo iptables -A INPUT -sport 22 
 #Only allow IP 130.15.100.100 to connect to your mysql running on port 3306
 
-#Drop outgoing ssh traffic to all other computers 
+#Drop outgoing ssh traffic to all other computers #compare with notebook 
+sudo iptables -A INPUT -p TCP -sport 22 -j DROP 
 
 #Can you still ssh the VM from your host machine? Why? If you cant, how to fix it?
-No.
+No. You must use port forwarding to allow the virtual machine to act like a normal guest on the home network. 
