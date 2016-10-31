7422537744fce1de4f588294e461490fa17e26cc
dseekatz <dseekatz@gmail.com>
2016-10-31 16:20:53 -0400

    Good enough? Very close certainly.

--- Question1.sh
+++ Question1.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Question1
+# This script lists all active processes.  If a process has children, then its children are listed underneath it.
+# Note that this is not a true process "tree", as many processes are listed more than once.
+
 # Get the list of processes and format it.
 oldIFS="$IFS"
 IFS=$'\n'
 processList=$(ps axo comm,pid,ppid,start,user | awk 'NR>1 {print $1 " PID:" $2 " PPID:" $3 " Started:" $4 " by:" $5}')
 
-# This will be the recursive function that sorts the processes into "tree" form.
-# Right now it just prints the full process list (unsorted)
+# This is the recursive function that prints the process list
 function processSort {
-	echo "$*"
+	for line in $* ; do
+		echo "$line"
+		pid=$(echo "$line" | awk '{print "P" $2}')		# Get the PID for the current process
+		myVar=$(echo "${processList[*]:1}" | grep "$pid")	# Get processes which have PPID=PID
+		processSort $myVar					# Run the function on those processes
+	done
 }
 
 processSort $processList
 
+#Put the IFS back to normal
 IFS=$oldIFS
