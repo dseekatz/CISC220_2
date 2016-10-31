d18baec127b339494da2ea37d8cb9ff09f7e0408
Brookealynn <brookeresendes@outlook.com>
2016-10-31 10:15:53 -0500

    q2

--- Question2.sh
+++ Question2.sh
@@ -1,7 +1,7 @@
 #Question2
 #!/bin/bash
 # This script backs up all the files in the repository, except if the file has previously been backed up, and pushes the backup to the github repository
-# cron job command is used to ensure the script runs everyday at 1:00am
+# cron job command is used to ensure the script runs everyday at 1:00am crontab -e 01*** 
 
 #Input path to repository folder, github URL, github username and password as parameters
 path=$1
@@ -13,25 +13,21 @@ password=$4
 # tar c:create, z:compress using gzip, v:verbose, f: into file
 
 # Naming backup files where the file name is backup, followed by the current year, month, day and hour
-TIME=$(date + %Y%m%d%-%H)
-
-#The filename will be "backup" and the year/month/day/hour that the file was backed up
-fileName=backup -$TIME.tar.gz
+year=$(date +'%Y')
+month=$(date +'%m')
+day=$(date +'%d')
+hour=$(date +'%H')
 
 #Compress files
-tar czvf $fileName . --exclude= backup*.tgz #previously backed up files with current dat
+tar -C $path -czvf backup$year$month$day$hour.tgz --exclude= *.tgz &> backupsLog #previously backed up files with current dat
 
 #Commit the new backup file to local git repository
-git add compressedFiles.tgz
-git commit compressedFiles.tgz
+git add backup$yeah$month$day$hour.tgz
+git commit -m "Comming the backup file to the git repository"
 
 #Push the new backup file to remote git repository
-git push $1 --all
+git push http://$3:$4@$2 --all 
 
 #Run the script everyday at 1:00AM and its output redirected to the backupsLog file
-#Create backupLog file
-$ cat > backupLog.sh
-echo The file has been backed up to the backupLog
-#Create run time: daily at 1:00am #can just put this in script?
-crontab -e 001*** echo >> backupLog.sh
+echo The file has been backed up to the backupLog file
 
